ada5fd80980bc366243da605c04de2335c94aae5
an-halim <halimbla2@gmail.com>
2024-06-13 10:30:44 +0700

    fix: crud user unittest

--- session-2-latihan-crud-unittest/middleware/auth_test.go
+++ session-2-latihan-crud-unittest/middleware/auth_test.go
@@ -45,6 +45,7 @@ func TestAuthMiddleware(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			tt := tt
 			t.Parallel()
 			// Create a new gin router with the AuthMiddleware applied
 			router := gin.New()
